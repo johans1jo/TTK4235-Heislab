c1dbebc47d68ffe626981d0ed6a51aad785cd231
Siri Hegsvold <sirihhe@stud.ntnu.no>
2019-03-24 14:57:01 +0100

    Opprettelse av headerfilene til Timer

---
+++ source/timer.c
@@ -0,0 +1,8 @@
+//
+//  timer.c
+//  
+//
+//  Created by Siri Hegsvold on 24/03/2019.
+//
+
+#include "timer.h"
---
+++ source/timer.h
@@ -0,0 +1,17 @@
+//
+//  timer.h
+//  
+//
+//  Created by Siri Hegsvold on 24/03/2019.
+//
+
+#ifndef timer_h
+#define timer_h
+
+#include <stdio.h>
+
+#endif /* timer_h */
+
+int start_timer();
+
+bool time_out();
