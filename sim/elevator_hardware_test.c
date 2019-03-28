60718bf818b2c1a1e146e482d35ce57d1f74ca89
Jo Aleksander Johansen <joaj@stud.ntnu.no>
2019-03-28 16:00:55 +0100

    more wrappings

--- sim/elevator_hardware_test.c
+++ sim/elevator_hardware_test.c
@@ -1,12 +1,11 @@
-#include "elevator_hardware.h"
+#include "elevsim.h"
 
 int main() {
-    elevator_hardware_init();
-
+    elev_init();
     while(1) {
-	elevator_hardware_set_motor_direction(DIRN_DOWN);
-	while(elevator_hardware_get_floor_sensor_signal() != 0) {}
-	elevator_hardware_set_motor_direction(DIRN_UP);
-	while(elevator_hardware_get_floor_sensor_signal() != 3) {}
+	elev_set_motor_direction(DIRN_DOWN);
+	while(elev_get_floor_sensor_signal() != 0) {}
+	elev_set_motor_direction(DIRN_UP);
+	while(elev_get_floor_sensor_signal() != 3) {}
     }
 }
--- sim/elevsim.h
+++ sim/elevsim.h
@@ -24,7 +24,7 @@ void elev_init();
 void elev_set_motor_direction(elev_motor_direction_t dirn);
 void elev_set_button_lamp(elev_button_type_t button, int floor, int value);
 void elev_set_floor_indicator(int floor);
-void elev_set_door_open_lamp((int value);
+void elev_set_door_open_lamp(int value);
 void elev_set_stop_lamp(int value);
 
 int elev_get_button_signal(elev_button_type_t button, int floor);
