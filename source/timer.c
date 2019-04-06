e3e45328bdf2ba2472d97c6e452f567ef7e06966
kennyhn <kennyhn@stud.ntnu.no>
2019-04-06 13:55:52 +0200

    oppdaterte kommentarer

--- source/timer.c
+++ source/timer.c
@@ -2,11 +2,11 @@
 
 
 void start_timer(){
-    before = time(NULL);
+    g_before = time(NULL);
 }
 int time_out(){
     int trigger = 3;
-    double sec = difftime(time(NULL), before);
+    double sec = difftime(time(NULL), g_before);
     if (sec < trigger){
         return 0;
     } else {
--- source/timer.h
+++ source/timer.h
@@ -1,13 +1,23 @@
+/**
+ * @file
+ * @brief A simple module to controll the timer of the door.
+ */ 
+
 #include <stdio.h>
 #include <time.h>
 
+//spør om dette er en global variabel - i såfall må den kommenteres
+clock_t g_before;
 
-
-
-
-
-clock_t before;
-
+/**
+ * @brief Sets the starttime to the current time.
+ */
 void start_timer(void);
 
+/**
+ * @brief Checks if it's three seconds since the starttime. 
+ *  
+ * @return 0 if it's not three seconds since the starttime.
+ * and 1 if it's three seconds or more since the starttime.
+ */
 int time_out(void);
